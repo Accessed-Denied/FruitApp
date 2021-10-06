f5135d34b4d9bc75c8fc70adc82e9d97a448eb32
sonusainier <rohitsaini@lambdatest.com>
2021-10-06 23:44:09 +0530

    Initial Commit

---
+++ FruitApp/FruitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitAppApp.swift
+//  FruitApp
+//
+//  Created by Rohit Saini on 06/10/21.
+//
+
+import SwiftUI
+
+@main
+struct FruitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
