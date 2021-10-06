d72adf2a894ca924054522ed2f569018d5032415
rohitsainier <rohitsainier@gmail.com>
2021-10-07 01:29:58 +0530

    Onboarding screen added

---
+++ FruitApp/App/OnboardingView.swift
@@ -0,0 +1,32 @@
+//
+//  OnboardingView.swift
+//  FruitApp
+//
+//  Created by Rohit Saini on 07/10/21.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    //MARK: - PROPERTIES
+    
+    //MARK: - BODY
+    var body: some View {
+        
+        TabView{
+            ForEach(0..<5){item in
+                FruitCardView()
+            }//: LOOP
+        }//: TABVIEW
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical,20)
+        
+    }
+}
+
+//MARK: - PREVIEW
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
